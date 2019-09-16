6e3ca9458b8a19cae2ca8ffdf079836319fefc44
achuth10 <43288837+achuth10@users.noreply.github.com>
2019-09-16 19:15:19 +0530

    Added bottom_Nav

--- app/src/main/java/com/example/dbms/Constants.java
+++ app/src/main/java/com/example/dbms/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public  static final String DB_URL = "https://dbmsprojamu.000webhostapp.com/";
     public  static final String REG_URL = DB_URL + "register.php";
     public  static final String LOGIN_URL = DB_URL + "login.php";
-
+    public  static final String TEST_URL = DB_URL + "TestConnection.php";
     public  static final String KEY_NAME = "name";
     public  static final String KEY_EMAIL = "email";
     public  static final String KEY_PASSWORD= "password";
