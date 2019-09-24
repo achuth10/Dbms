e192bbcff61b9d980ff78bf7394cd3929e19a409
achuth10 <43288837+achuth10@users.noreply.github.com>
2019-09-24 20:35:14 +0530

    Added prediction

--- app/src/main/java/com/example/dbms/Fragments/HomePage.java
+++ app/src/main/java/com/example/dbms/Fragments/HomePage.java
@@ -1,18 +1,17 @@
 package com.example.dbms.Fragments;
 
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -20,16 +19,17 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-import com.example.dbms.Activities.Home;
 import com.example.dbms.Models.Constants;
 import com.example.dbms.Models.Crop;
-import com.example.dbms.Models.CropAdapter;
+import com.example.dbms.Models.IdealAdapter;
 import com.example.dbms.Models.MySingleton;
+import com.example.dbms.Models.PhAdapter;
+import com.example.dbms.Models.RainAdapter;
+import com.example.dbms.Models.TempAdapter;
 import com.example.dbms.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 
@@ -37,9 +37,13 @@ import java.util.Map;
 public class HomePage extends Fragment {
     private SharedPreferences pref ;
     private SharedPreferences.Editor editor ;
-    private Button GetPhCrop;
+    private IdealAdapter idealAdapter;
+    private RainAdapter rainAdapter;
+    private PhAdapter phAdapter;
+    private TempAdapter tempAdapter;
     private ArrayList<Crop> crops,idealcrops,phcrops,tempcrops,raincrops;
     private String[] idealcropslist ,phcropslist ,tempcropslist ,raincropslist ,items;
+    private RecyclerView idealR,phR,tempR,rainR;
     public HomePage() {
         // Required empty public constructor
         setRetainInstance(true);
@@ -53,14 +57,22 @@ public class HomePage extends Fragment {
         View  v = inflater.inflate(R.layout.fragment_home_page, container, false);
         pref = getActivity().getSharedPreferences("MyPref", 0); // 0 - for private mode;
         editor = pref.edit();
-        GetPhCrop = v.findViewById(R.id.GetPhCrop);
-        GetPhCrop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getphcrops();
-            }
-        });
+        idealR = v.findViewById(R.id.IdealRecycler);
+        phR= v.findViewById(R.id.PhRecycler);
+        tempR= v.findViewById(R.id.TempRecycler);
+        rainR = v.findViewById(R.id.RainRecycler);
+idealcrops=new ArrayList<>();
+phcrops=new ArrayList<>();
+tempcrops=new ArrayList<>();
+raincrops=new ArrayList<>();
+
         return  v;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getphcrops();
     }
 
     private void getphcrops() {
@@ -70,7 +82,29 @@ public class HomePage extends Fragment {
         StringRequest request = new StringRequest(Request.Method.POST, Constants.SUITABLECROPS_URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                Toast.makeText(getContext(),response.toString(), Toast.LENGTH_LONG).show();
+                if(idealcropslist!=null)
+                {
+                    idealcropslist = null;
+                    idealcrops.clear();
+                }
+                if(raincropslist!=null)
+                {
+                    raincropslist = null;
+                    raincrops.clear();
+                }
+
+                if(phcropslist!=null)
+                {
+                    phcropslist = null;
+                    phcrops.clear();
+                }
+
+                if(tempcropslist!=null)
+                {
+                    tempcropslist = null;
+                    tempcrops.clear();
+                }
+                //Toast.makeText(getContext(),response.toString(), Toast.LENGTH_LONG).show();
                 System.out.println(" crops is : " + response.toString());
                 items = response.split("-");
                 for (String item : items) {
@@ -97,37 +131,70 @@ public class HomePage extends Fragment {
 
 
                 }
+
+
                 System.out.println("Ideal crops ");
                 for (String s : idealcropslist) {
                     System.out.println(s);
                     idealcrops.add(new Crop(s.trim()));
                 }
+                idealAdapter = new IdealAdapter(getContext(),idealcrops);
+
+
+
 
                 System.out.println("ph crops " );
                 for (String s : phcropslist) {
                     System.out.println(s);
                     phcrops.add(new Crop(s.trim()));
                 }
+                phAdapter = new PhAdapter(getContext(),phcrops);
+
+
+
+
+
                 System.out.println("temp crops " );
                 for (String s : tempcropslist) {
                     System.out.println(s);
                     tempcrops.add(new Crop(s.trim()));
                 }
+                tempAdapter = new TempAdapter(getContext(),tempcrops);
+
+
+
                 System.out.println("rain crops " );
                 for (String s : raincropslist) {
                     System.out.println(s);
                     raincrops.add(new Crop(s.trim()));
                 }
+                rainAdapter = new RainAdapter(getContext(),raincrops);
+
+
+
+
+
+
                 if (getActivity()!=null) {
-//                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-//                            getActivity(), android.R.layout.simple_spinner_item, spinnerArray);
-//
-//                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//                    spinner.setAdapter(adapter);
 
 
-                    dispadapter(crops);
-                   // progressBar.setVisibility(View.INVISIBLE);
+                        idealR.setLayoutManager(new LinearLayoutManager(getContext()));
+                        idealR.setAdapter(idealAdapter);
+                        idealAdapter.notifyDataSetChanged();
+
+
+                        phR.setLayoutManager(new LinearLayoutManager(getContext()));
+                        phR.setAdapter(phAdapter);
+                        phAdapter.notifyDataSetChanged();
+
+                        tempR.setLayoutManager(new LinearLayoutManager(getContext()));
+                        tempR.setAdapter(tempAdapter);
+                        tempAdapter.notifyDataSetChanged();
+
+
+                        rainR.setLayoutManager(new LinearLayoutManager(getContext()));
+                        rainR.setAdapter(rainAdapter);
+                        rainAdapter.notifyDataSetChanged();
                 }
 
             }
@@ -151,10 +218,4 @@ public class HomePage extends Fragment {
 
     }
 
-    private void dispadapter(ArrayList<Crop> crops) {
-//        cropAdapter = new CropAdapter(getContext(),crops);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-//        recyclerView.setAdapter(cropAdapter);
-//        cropAdapter.notifyDataSetChanged();
-    }
 }
---
+++ app/src/main/java/com/example/dbms/Models/TempAdapter.java
@@ -0,0 +1,67 @@
+package com.example.dbms.Models;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dbms.Activities.CropView;
+import com.example.dbms.R;
+
+import java.util.ArrayList;
+
+public class TempAdapter extends RecyclerView.Adapter <TempAdapter.CropHolder>{
+    public Context context;
+    private  ArrayList<Crop> cropPOJS;
+
+
+    public TempAdapter(Context context, ArrayList<Crop> cropPOJS) {
+        this.context = context;
+        this.cropPOJS = cropPOJS;
+    }
+
+    @NonNull
+    @Override
+    public CropHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.croplist, parent, false);
+        return new CropHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CropHolder holder, int position) {
+        Crop crop = cropPOJS.get(position);
+        holder.setdetails(crop,context);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cropPOJS.size();
+    }
+
+    public static class CropHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView name,itemno,price;
+        private Context context;
+        private String Cropname;
+        CropHolder( View itemView) {
+            super(itemView);
+            context=itemView.getContext();
+            name = itemView.findViewById(R.id.CropNameListTxt);
+        }
+        void setdetails(Crop crop, Context context) {
+            Cropname = crop.getCrop_name();
+            name.setText(Cropname);
+            name.setOnClickListener(this);
+        }
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(context, CropView.class);
+            i.putExtra("Crop",Cropname);
+            context.startActivity(i);
+        }
+    }
+}
