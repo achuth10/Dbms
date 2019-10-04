45ab2c622dde0dc2eaaab0861b7e502aad368cd5
achuth10 <43288837+achuth10@users.noreply.github.com>
2019-10-04 22:24:23 +0530

    Bug fixes

--- app/src/main/java/com/example/dbms/Models/Crop.java
+++ app/src/main/java/com/example/dbms/Models/Crop.java
@@ -2,9 +2,6 @@ package com.example.dbms.Models;
 
 public class Crop {
     private String crop_name;
-    private String min_ph;
-    private String max_ph;
-    private String min_rainfall;
     private String url;
 
     public Crop(String s, String s1) {
@@ -12,57 +9,17 @@ public class Crop {
         url = s1;
     }
 
-    public String getUrl() {
+    String getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
-    }
 
     public Crop(String name)
 {
     this.crop_name = name;
 }
-    public String getCrop_name() {
+    String getCrop_name() {
         return crop_name;
     }
 
-    public void setCrop_name(String crop_name) {
-        this.crop_name = crop_name;
-    }
-
-    public String getMin_ph() {
-        return min_ph;
-    }
-
-    public void setMin_ph(String min_ph) {
-        this.min_ph = min_ph;
-    }
-
-    public String getMax_ph() {
-        return max_ph;
-    }
-
-    public void setMax_ph(String max_ph) {
-        this.max_ph = max_ph;
-    }
-
-    public String getMin_rainfall() {
-        return min_rainfall;
-    }
-
-    public void setMin_rainfall(String min_rainfall) {
-        this.min_rainfall = min_rainfall;
-    }
-
-    public String getMax_rainfal() {
-        return max_rainfal;
-    }
-
-    public void setMax_rainfal(String max_rainfal) {
-        this.max_rainfal = max_rainfal;
-    }
-
-    private String max_rainfal;
 }
